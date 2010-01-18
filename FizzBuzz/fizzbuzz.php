55863811e5d53b64a8fe77b1aa26a07f2b7a912a
Macha <macha.hack@gmail.com>
2010-01-19 05:14:33 +0800

    Added FizzBuzz program (and a few languages - too busy to add more)

---
+++ FizzBuzz/fizzbuzz.php
@@ -0,0 +1,13 @@
+<?php
+	for($i = 1; $i <= 100; $i++) {
+		if(($i % 5 == 0) && ($i % 3 == 0)) {
+			echo "FizzBuzz" . "\n";			
+		} elseif($i % 3 == 0) {
+			echo "Fizz" . "\n";	
+		} elseif($i % 5 == 0) {
+			echo "Buzz" . "\n";	
+		} else {
+			echo $i . "\n";
+		}
+	}
+?>
